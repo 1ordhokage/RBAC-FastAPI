fcd8c25ef5b5bbc5807733c9f1e6dc8fa182e3b1
Jurabek Aminov <dzhurabek.aminov@gmail.com>
2023-03-08 18:14:57 +0300

    Added config files

---
+++ src/core/admin_settings.py
@@ -0,0 +1,14 @@
+from pydantic import BaseSettings
+
+
+class AdminSettings(BaseSettings):
+    """Admin config class."""
+    login: str
+    password: str
+
+    class Config:
+        env_file = '../.env'
+        env_prefix = "ADMIN_"
+
+
+admin_settings = AdminSettings()
